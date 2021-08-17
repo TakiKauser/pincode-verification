6cf3b991b3faa4acd8c1cc0dd9ca923095552fd7
TakiKauser <takikauserbadhon@gmail.com>
2021-08-18 05:16:50 +0600

    code generator works!

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-console.log("Connected!");
+// pincode generator
+function pinGenerator(){
+    // generating pincode
+    const generatedPin = Math.round(Math.random() * 1000000);
+    // ensuring digits' count or handling errors
+    if ((generatedPin + "").length == 6){
+        return generatedPin;
+    }
+    return pinGenerator();
+}
+document.getElementById("pin-generator").addEventListener("click",function(){
+    // getting display field
+    const generatedPincodeDisplay = document.getElementById("generated-pincode");
+    // showing generated pincode in display
+    const generatedPincode = pinGenerator();
+    generatedPincodeDisplay.value = generatedPincode;
+});
