a6f6c5244427d62e795717b0c185ae69ec79264f
TakiKauser <takikauserbadhon@gmail.com>
2021-08-18 06:37:41 +0600

    pressed-key displayed

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,21 @@ document.getElementById("pin-generator").addEventListener("click",function(){
     const generatedPincode = pinGenerator();
     generatedPincodeDisplay.value = generatedPincode;
 });
+
+document.getElementById("key-pad").addEventListener("click", function(event){
+    const pressedKey = event.target.innerText;
+    const displayedInput = document.getElementById("display");
+    const display = document.getElementById("display").value;
+    if (isNaN(pressedKey)){
+        if (pressedKey == "C"){
+            displayedInput.value = "";
+        }
+        else if (pressedKey == "<"){
+            displayedInput.value = display.substring(0, display.length - 1);
+        }
+    }
+    else{     
+        const userInput = display + pressedKey;
+        displayedInput.value = userInput;
+    }
+});
